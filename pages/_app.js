8abd0af4d192753e8a18c76fb1d0459fbaae07a5
Michael Hajný <michael.hajny@precismo.com>
2023-04-18 15:00:00 +0200

    posting on vercel server

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { GlobalStyle } from '../styles/global.style'
 import Notification from '../components/Notification'
+import axios from 'axios'
+
+// check if production or local
+
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'https://batchelor-thesis-testing.vercel.app/api/'
+} else{
+  axios.defaults.baseURL = 'https://localhost:3000/api/'
+}
 
 // basic component of react and nextjs
 function MyApp({ Component, pageProps }) {
